24026a5c8a56068c66d51f6fb5e756685a14ecf0
Srisivalakshmi23 <srisivalakshmi.p06@gmail.com>
2025-05-14 14:11:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/For Loop- primeCheck/for-loop-prime-check.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            String res = obj.isPrime(n);
+            
+            System.out.println(res);
+            
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static String isPrime(int n) {
+
+        // Code here
+        if (n <= 1) {
+            return "No";
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0){
+                return "No";
+            }
+            // Write your logic here
+            // Return "Yes" if n is prime, else return "No"
+            /*n is prime only if it is not divisible by any i.
+            We will not reach till n and 1 is already discarded*/
+            // return  Yes if n is a prime number ,else return No
+        }
+        return "Yes";
+    }
+}
