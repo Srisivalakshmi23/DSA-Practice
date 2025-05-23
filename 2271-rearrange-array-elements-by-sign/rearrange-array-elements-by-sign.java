f92f4b5062d6ddc5eae3da7f193edcbe03f6c58a
Srisivalakshmi23 <srisivalakshmi.p06@gmail.com>
2025-05-23 10:25:25 +0530

    Time: 3 ms (100.00%) | Memory: 77.1 MB (57.07%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,25 +1,46 @@
+// class Solution {
+//     public int[] rearrangeArray(int[] nums) {
+//         int n = nums.length;
+//         int[] result = new int[n];
+//         List<Integer> pos = new ArrayList<>();
+//         List<Integer> neg = new ArrayList<>();
+//         for(int num : nums){
+//             if(num > 0){
+//                 pos.add(num);
+//             } else {
+//                 neg.add(num);
+//             }
+//         }
+//         int posIndex = 0;
+//         int negIndex = 0;
+//         for(int i = 0; i < n; i++){
+//             if(i % 2 == 0){
+//                 result[i] = pos.get(posIndex++);
+//             } else {
+//                 result[i] = neg.get(negIndex++);
+//             }
+//         }
+//         return result;
+//     }
+// }
+
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
         int[] result = new int[n];
-        List<Integer> pos = new ArrayList<>();
-        List<Integer> neg = new ArrayList<>();
-        for(int num : nums){
-            if(num > 0){
-                pos.add(num);
-            } else {
-                neg.add(num);
-            }
-        }
         int posIndex = 0;
-        int negIndex = 0;
-        for(int i = 0; i < n; i++){
-            if(i % 2 == 0){
-                result[i] = pos.get(posIndex++);
+        int negIndex = 1;
+
+        for (int num : nums) {
+            if (num > 0) {
+                result[posIndex] = num;
+                posIndex += 2;
             } else {
-                result[i] = neg.get(negIndex++);
+                result[negIndex] = num;
+                negIndex += 2;
             }
         }
+
         return result;
     }
 }
