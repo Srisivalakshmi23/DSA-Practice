914113fa9cb46332e2dcd01ba97d7f084dc7db50
Srisivalakshmi23 <srisivalakshmi.p06@gmail.com>
2025-06-24 12:36:46 +0530

    Time: 2563 ms (5.01%) | Memory: 56.3 MB (51.20%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,6 +1,16 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        // return nums;
+        for(int i = 1; i < nums.length; i++){
+            int j = i - 1;
+            int key = nums[i];
+            while(j >= 0 && key < nums[j]){
+                nums[j+1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
+        }
         return nums;
     }
-}
+} 
